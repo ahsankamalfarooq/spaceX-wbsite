4bd069cac83918cd6436e8f1924b3a81f5613d2f
Wazir <ahsankamal25@gmail.com>
2023-05-14 15:56:42 +0500

    sec 1 complted with err

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
     let socials = document.querySelectorAll('.social div')
 
     socials.forEach(function(social, index) {
-        social.style.animation = 'moveIn 1s ease-in-out forwords 2s'
+        social.style.animation = `moveIn .6s cubic-bezier(.51,.92,.24,1.15) forwards ${index/7 + 0.2}s`
     })
 
 }())
